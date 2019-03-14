fd87ca4ab3030df838e4ce7dbdbf92e592bc021d
Kursaitova <kz.asie.zk@mail.ru>
2019-03-14 22:34:36 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pokemon/precache-manifest.e5f59600684f372b146ec6368e83ce92.js"
+  "/pokemon-task/precache-manifest.eafbe803d8530ad806bfb8b481065867.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pokemon/index.html", {
+workbox.routing.registerNavigationRoute("/pokemon-task/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
